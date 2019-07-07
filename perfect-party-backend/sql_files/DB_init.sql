afd599ca6eddcfc7fad1235b76ee91b380d8c247
Mingran Zhou <zfrank1998@gmail.com>
2019-07-07 13:47:56 -0400

    correct init file

--- perfect-party-backend/sql_files/DB_init.sql
+++ perfect-party-backend/sql_files/DB_init.sql
@@ -5,16 +5,17 @@ CREATE TABLE perfect_party_db.Client(
 	FName varchar(64) NOT NULL,
 	LName varchar(64) NOT NULL,
 	Email varchar(64),
+	Active boolean DEFAULT 1,
 	PRIMARY KEY (ClientID)
 );
 
 CREATE TABLE perfect_party_db.Event(
 	EventID int NOT NULL AUTO_INCREMENT,
-	ClientID int NOT NULL,
-	TypeID int NOT NULL,
+	ClientID int,
+	TypeID int,
 	Budget decimal(20,2) NOT NULL,
 	EventDate date,
-	VenueID int NOT NULL,
+	VenueID int,
 	NumInvitees int DEFAULT 0,
 	PRIMARY KEY (EventID)
 );
@@ -23,6 +24,7 @@ CREATE TABLE perfect_party_db.EventType(
 	TypeID int NOT NULL AUTO_INCREMENT,
 	TypeName varchar(64) NOT NULL,
 	BasePrice decimal(20,2) NOT NULL,
+	Active boolean DEFAULT 1,
 	PRIMARY KEY (TypeID)
 );
 
@@ -34,36 +36,72 @@ CREATE TABLE perfect_party_db.Supplier(
 );
 
 CREATE TABLE perfect_party_db.Venue(
-	VenueID int NOT NULL AUTO_INCREMENT,
+  VenueID int NOT NULL AUTO_INCREMENT,
   Name varchar(64) NOT NULL,
   Address varchar(128) NOT NULL,
   Price decimal(20,2) NOT NULL,
+  Active boolean DEFAULT 1,
   PRIMARY KEY (VenueID)
 );
 
 CREATE TABLE perfect_party_db.Payment(
-	EventID int NOT NULL,
-	Total decimal(20,2) NOT NULL
+    PaymentID int NOT NULL AUTO_INCREMENT,
+	EventID int,
+	Total decimal(20,2) NOT NULL,
+	PRIMARY KEY (PaymentID)
 );
 
 CREATE TABLE perfect_party_db.PayItem(
 	ItemID int NOT NULL AUTO_INCREMENT,
-	ItemName varchar(64),
+	ItemName varchar(64) NOT NULL,
 	ItemType enum('food','decors','host','entertainment','other'),
-	ItemPrice decimal(20,2),
-	PRIMARY KEY (ItemID)
+	ItemPrice decimal(20,2) NOT NULL,
+    SupplierID int,
+    Active boolean DEFAULT 1,
+    PRIMARY KEY (ItemID)
 );
 
 CREATE TABLE perfect_party_db.ItemSelectRecord(
-	EventID int NOT NULL,
-	ItemID int NOT NULL,
-	Amount int NOT NULL DEFAULT 0
+    RecordID int NOT NULL AUTO_INCREMENT,
+	EventID int,
+	ItemID int,
+	Amount int NOT NULL DEFAULT 0,
+	PRIMARY KEY (RecordID)
 );
 
-ALTER TABLE perfect_party_db.Event ADD FOREIGN KEY (ClientID) REFERENCES Client(ClientID);
-ALTER TABLE perfect_party_db.Event ADD FOREIGN KEY (VenueID) REFERENCES Venue(VenueID);
-ALTER TABLE perfect_party_db.Event ADD FOREIGN KEY (TypeID) REFERENCES EventType(TypeID);
-ALTER TABLE perfect_party_db.Payment ADD FOREIGN KEY (EventID) REFERENCES Event(EventID);
-ALTER TABLE perfect_party_db.ItemSelectRecord ADD FOREIGN KEY (EventID) REFERENCES Event(EventID);
-ALTER TABLE perfect_party_db.ItemSelectRecord ADD FOREIGN KEY (ItemID) REFERENCES PayItem(ItemID);
+use perfect_party_db;
 
+ALTER TABLE Event ADD FOREIGN KEY (ClientID)
+	REFERENCES Client(ClientID)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE;
+
+ALTER TABLE Event ADD FOREIGN KEY (VenueID)
+	REFERENCES Venue(VenueID)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE;
+
+ALTER TABLE Event ADD FOREIGN KEY (TypeID)
+	REFERENCES EventType(TypeID)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE;
+
+ALTER TABLE Payment ADD FOREIGN KEY (EventID)
+	REFERENCES Event(EventID)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+
+ALTER TABLE PayItem ADD FOREIGN KEY (SupplierID)
+	REFERENCES Supplier(SupplierID)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE;
+
+ALTER TABLE ItemSelectRecord ADD FOREIGN KEY (EventID)
+	REFERENCES Event(EventID)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+
+ALTER TABLE ItemSelectRecord ADD FOREIGN KEY (ItemID)
+	REFERENCES PayItem(ItemID)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE;
